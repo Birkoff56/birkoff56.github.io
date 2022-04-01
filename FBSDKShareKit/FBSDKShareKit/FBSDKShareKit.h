884cc3b5317e4c8b52685c13455404a84b2c16b2
Birkoff56 <96592819+Birkoff56@users.noreply.github.com>
2022-04-01 23:48:07 +0000

    Initialize imgbot

---
+++ FBSDKShareKit/FBSDKShareKit/FBSDKShareKit.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <FBSDKShareKit/FBSDKHashtag.h>
+#import <FBSDKShareKit/FBSDKShareErrorDomain.h>
+#import <FBSDKShareKit/FBSDKShareMedia.h>
+#import <FBSDKShareKit/FBSDKSharePhoto.h>
+#import <FBSDKShareKit/FBSDKShareVideo.h>
+#import <FBSDKShareKit/_FBSDKShareDefines.h>
+#import <FBSDKShareKit/_FBSDKShareUtility.h>
+#import <FBSDKShareKit/_FBSDKShareUtilityProtocol.h>
+
+#if !TARGET_OS_TV
+ #import <FBSDKShareKit/_FBSDKSocialComposeViewController.h>
+#endif
